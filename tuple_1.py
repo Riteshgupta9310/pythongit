4cfa6c8fda1fe5dcb79e83d9159cd155202d08db
Riteshgupta9310 <115911203+Riteshgupta9310@users.noreply.github.com>
2022-12-09 23:56:16 +0530

    Add files via upload

---
+++ tuple_1.py
@@ -0,0 +1,89 @@
+#tuples - store mulyiple items in a single variable
+#orderded
+#unchangeable/unmutable
+#allow duplicate values
+#print(myTuple)
+
+#myTuple = (1,2,3,4,4)
+#print(myTuple)
+
+#tuple1 = ("apple"1,,1.1)
+#print(tuple1)
+
+#mytuple1 = ("cars","bike","boat","jeep")
+#print(mytuple[1:3])
+
+#tuple1 = ('cars','bike','boat','jeep')
+#list1 = list(tuple1)
+#list1.append = (cycle)
+#tuple2 = tuple(list1)
+#print(tuple1)
+
+#tuple = (10,20,30,40,50,60)
+#list1 = list(tuple)
+#list1.reverse = (30)
+#print(tuple)
+
+#tuple1 = ("Ritesh gupta")
+#print(tuple1)
+
+#tuple1 = ('apple','ornge','pea','dates')
+#del tuple1
+#print(tuple1)
+
+#tuple1 = (1,2,3,[6,7],4,5)
+#print(tuple1[3][0],tuple1[4])
+
+#tuple1 = (1,2,3,[6,7],4,5)
+#x=list(tuple1)
+#x[3][0]=8
+#print(tuple1)
+
+#tuple = (10,20)
+#tuple1 = (30,40)
+                #swap the tuples
+#print(tuple)
+#print(tuple1)
+
+#packaking and unpacking
+#tuple1 = (1,2,3)
+#(one,two ,three) = tuple1
+#print(one)
+#print(two)
+#print(three)
+
+
+#tuple1 = ("cars","bike","boat",'cycle','scooter',)
+#(item1,*item2 ,item3) = tuple1
+#print(item1)
+#print(item2)
+
+# forloop
+# (Question) write aprogram to unpack folowing tuple  into 4 variable
+#tuple1 =  (10,20,30,40,50)
+#tuple1 =  (10,20,30,40,50)
+#for i in tuple1:
+    #print(i)
+
+#while loop
+#tuple1 = (10,20,30,40,50,60,)
+#i = 0
+#while i < len(tuple1):
+    #print(tuple1[i])
+    #i += 1
+
+
+
+#tuple1 = (10,20,30,40,50,60,)
+#i = 0
+#while i < len(tuple1):
+    #x=(tuple1.index(20,30) )
+    #print(x)
+    #i += 1
+
+
+#output - hello
+tuple1 = ("H","E","L","L","O")
+for i in tuple1:
+    print(i, end='')
+    
